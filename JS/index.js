41989f1172ebd6528edc5226c65e1dd44de411b7
[Gabriel] <[0246183@up.edu.mx]>
2023-09-26 17:09:30 -0600

    actualizacion

--- JS/index.js
+++ JS/index.js
@@ -2,3 +2,23 @@ function addition(){
     var res = 5+6;
     alert(res);
 }
+
+function callAction(){
+    document.getElementsByTagName("h1")[0].
+    innerHTML = "Goodbye pals"
+
+    var special = document.getElementsByClassName("special");
+    for(i=0; i < specials.lenght; i++)
+    special[i].innerHTML = "<b>Have a wonerful day</b>";
+
+    document.getElementById("first").innerHTML="i'm the one";
+
+    var liElements = document.querySelectorAll(".list li");
+    for(i=0; i<liElements.length; i++){
+        liElements[i].style.color="green";
+    }
+
+    document.querySelector(".list #first").classList.add
+    ("highlight");
+    
+}
