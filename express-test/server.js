5a19d691583479c03d0dfb585cb45f36a1ac36c6
[Gabriel] <[0246183@up.edu.mx]>
2023-10-12 09:14:53 -0600

    actualizacion

--- express-test/server.js
+++ express-test/server.js
@@ -1,18 +1,48 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.engine("ejs",require("ajs").renderFile);
+app.set("view engine", "ejs");
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+  res.sendFile(__dirname + "/public/html/index.html");
+});
 
-app.get('/about', (req, res) => {
-    res.send("It's all about the soul");
+app.get("/about", (req, res) => {
+  var name = req.query.name;
+  console.log(name);
+  res.send("<h1>Hello " + name + ". </h1>");
+});
+
+app.get("/about/:name/detail", (req, res) => {
+  var name = req.params.name;
+  console.log(name);
+  res.send("<h1>Hello " + name + " from parameter in URL. </h1>");
+});
+
+app
+  .route('/login')
+  .get((req, res) => {
+    var username = req.query.username;
+    var password = req.query.password;
+
+    res.send("<h1>Hello " + username + " you are now logged in</h1>");
   })
+  .post((req, res) => {
+    var username = req.body.username;
+    var password = req.body.password;
+
+    res.send("<h1>Hello " + username + " you are now logged in POST</h1>");
+  });
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
+
 
 app.get('/secure', (req, res) => {
-    res.send("This is server location");
-  })
-
-app.listen(3000,() =>{
-    console.log("Listening on port 3000");
-})
+  res.send("This is a secure location");
+});
